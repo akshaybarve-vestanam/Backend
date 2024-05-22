b0b7775e1dfb1fa6ebfa78b43fb97eeb72f19f5b
Akshay barve <akshay.barve@vestanam.com>
2024-05-22 22:13:21 +0530

    candidate 2

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,6 @@ const user = require("../app/controller/user.js");
 const labels = require("../app/controller/labels.js");
 const candidates = require("../app/controller/candidates.js")
 
-router.post("/signup", user.signup);
 router.get("/labels", labels.get);
 router.post("/labels", labels.create);
 router.post("/candidate/register/individual", candidates.candidates_register);
