8241600a8510e44cc8a07acaaee9fa14aae7dd81
Devavrat304 <devavrat.thokal@vestanam.com>
2024-07-04 13:31:04 +0530

    swagger and auth.js changes

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const user = require("../app/controller/user.js");
 const labels = require("../app/controller/labels.js");
 const candidates = require("../app/controller/candidates.js")
+const companies = require("../app/controller/companies.js");
 
 router.get("/labels", labels.get);
 router.post("/labels", labels.create);
@@ -10,4 +11,5 @@ router.post("/candidate/register/individual", candidates.candidates_register);
 router.post("/candidate/edit", candidates.candidates_edit);
 router.get("/candidate",candidates.load_candidates);
 router.get("/download/:candidateId", candidates.download_candidate_data);
+router.post("/companies/register", companies.companies_register);
 module.exports = router;
