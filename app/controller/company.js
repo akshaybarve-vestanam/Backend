6c509e789ea2983547b9cdf257f6a9e45a117ebe
OmGattuwar <om.gattuwar@vestanam.com>
2024-07-09 11:53:02 +0530

    fixed load company api

--- app/controller/company.js
+++ app/controller/company.js
@@ -38,6 +38,45 @@ module.exports.companies_register = async (req, res) => {
 //   };
 
 
+// module.exports.load_companies = async (req, res) => {
+//   try {
+//       let query = {};
+
+//       if (req.query.search) {
+//           query = {
+//               $or: [
+//                   { name: { $regex: new RegExp(req.query.search, 'i') } },
+//                   { city: { $regex: new RegExp(req.query.search, 'i') } },
+//                   { country: { $regex: new RegExp(req.query.search, 'i') } }
+//               ]
+//           };
+//       }
+
+//       // Pagination parameters
+//       const offset = parseInt(req.query.offset) || 0; // default to 0 if not provided
+//       const limit = parseInt(req.query.limit) || 10; // default to 10 if not provided
+
+//       // Get total count of companies matching the query
+//       const count = await Company.countDocuments(query);
+
+//       // Fetch companies with pagination
+//       const companies = await Company.find(query).skip(offset).limit(limit);
+
+
+      
+      
+                                    
+
+//       res.json({ s: true, d: companies, m: "Companies List", count: count });
+//   } catch (error) {
+//       console.error("Error loading companies:", error);
+//       res.status(500).json({ s: false, m: "Error loading companies" });
+//   }
+// };
+
+
+
+
 module.exports.load_companies = async (req, res) => {
   try {
       let query = {};
@@ -52,20 +91,21 @@ module.exports.load_companies = async (req, res) => {
           };
       }
 
-      // Pagination parameters
-      const offset = parseInt(req.query.offset) || 0; // default to 0 if not provided
-      const limit = parseInt(req.query.limit) || 10; // default to 10 if not provided
+      // Check for pagination parameters
+      const offset = parseInt(req.query.offset);
+      const limit = parseInt(req.query.limit);
 
       // Get total count of companies matching the query
       const count = await Company.countDocuments(query);
 
-      // Fetch companies with pagination
-      const companies = await Company.find(query).skip(offset).limit(limit);
-
-
-      
-      
-                                    
+      let companies;
+      if (!isNaN(offset) && !isNaN(limit)) {
+          // Fetch companies with pagination if offset and limit are provided
+          companies = await Company.find(query).skip(offset).limit(limit);
+      } else {
+          // Fetch all companies if no pagination parameters are provided
+          companies = await Company.find(query);
+      }
 
       res.json({ s: true, d: companies, m: "Companies List", count: count });
   } catch (error) {
