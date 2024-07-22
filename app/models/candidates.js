a2732d65b8ce6200b2e5897e94f4c34547d2c750
OmGattuwar <om.gattuwar@vestanam.com>
2024-07-22 17:40:57 +0530

    populating candidate collection

--- app/models/candidates.js
+++ app/models/candidates.js
@@ -4,16 +4,183 @@
 
 
 
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// const ObjectId = Schema.Types.ObjectId;
+ 
+// let validateEmail = (email) => {
+//   if (!email || email === '') return true; // Allow empty emails
+//   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   return re.test(email);
+// };
+ 
+// const CandidateSchema = new Schema(
+//   {
+//     candidateId: {
+//       type: String,
+//       unique: true,
+//     },
+//     selectedTestType: {
+//       type: String,
+//       required: true,
+//     },
+//     fullName: {
+//       type: String,
+//       required: [true, "First name is missing"],
+//       maxlength: 100,
+//     },
+//     phoneNumber: {
+//       type: String,
+//       maxlength: 20,
+//     },
+//     email: {
+//       type: String,
+//       trim: true,
+//       lowercase: true,
+//       validate: {
+//         validator: validateEmail,
+//         message: "Please fill a valid email address",
+//       },
+//     },
+//     selectedLabels: {
+//       type: Array,
+//     },
+//     testDateTime: {
+//       type: Date,
+//     },
+
+//     firstName: {
+//       type: String,
+//     },
+//     middleName: {
+//       type: String,
+//     },
+//     lastName: {
+//       type: String,
+//     },
+//     status: {
+//       type: String,
+//     },
+//     category: {
+//       type: String,
+//     },
+//     messageCount: {
+//       type: Number,
+//     },
+//     name: {
+//       type: String,
+//     },
+//     countryCode: {
+//       type: Number,
+//     },
+//     project: {
+//       type: Schema.Types.Mixed,
+//     },
+//     belongTo: {
+//       type: ObjectId,
+//       ref: "Registrations",
+//     },
+//     user: {
+//       type: ObjectId,
+//       ref: "Registrations",
+//     },
+  
+//     education: {
+//       type: Array,
+//     },
+//     examId: [{
+//       type: ObjectId,
+//       ref: "Users",
+//     }],
+//     fName: {
+//       type: String,
+//     },
+//     mName: {
+//       type: String,
+//     },
+//     lName: {
+//       type: String,
+//     },
+//     gender: {
+//       type: String,
+//     },
+//     dob: {
+//       type: Date,
+//     },
+//     curriculum: {
+//       type: String,
+//     },
+//     city: {
+//       type: String,
+//     },
+//     pincode: {
+//       type: String,
+//     },
+//     state: {
+//       type: String,
+//     },
+//     mobileNo: {
+//       type: Number,
+//     },
+//     belongTo: {
+//       type: ObjectId,
+//       ref: "Users",
+//     },
+//     examName: {
+//       type: ObjectId,
+//       ref: "Users",
+//     },
+//     reg: {
+//       type: ObjectId,
+//       ref: "Users",
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+ 
+// CandidateSchema.pre("save", async function (next) {
+//   console.log("candidate pre save hook called");
+//   const chars = "0123456789";
+//   let candidateid = "";
+//   let isUnique = false;
+ 
+//   while (!isUnique) {
+//     candidateid = "S";
+//     for (let i = 0; i < 6; i++) {
+//       candidateid += chars.charAt(Math.floor(Math.random() * chars.length));
+//     }
+ 
+//     const existingCandidate = await Candidate.findOne({ candidateid });
+//     if (!existingCandidate) {
+//       isUnique = true;
+//     } else {
+//       console.log(`Duplicate candidate ID found: ${candidateid}. Generating a new ID.`);
+//     }
+//   }
+//   console.log("candidateid");
+//   this.candidateId = candidateid;
+//   next();
+// });
+ 
+// const Candidate = mongoose.model("Candidate", CandidateSchema);
+ 
+// module.exports = Candidate;
+
+
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
- 
+
 let validateEmail = (email) => {
-  if (!email || email === '') return true; // Allow empty emails
+  if (!email) return true; // Allow empty or undefined emails
+  if (email === 'unknown') return true; // Explicitly allow 'unknown' value
   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return re.test(email);
 };
- 
+
 const CandidateSchema = new Schema(
   {
     candidateId: {
@@ -48,7 +215,6 @@ const CandidateSchema = new Schema(
     testDateTime: {
       type: Date,
     },
-
     firstName: {
       type: String,
     },
@@ -84,7 +250,6 @@ const CandidateSchema = new Schema(
       type: ObjectId,
       ref: "Registrations",
     },
-  
     education: {
       type: Array,
     },
@@ -139,19 +304,19 @@ const CandidateSchema = new Schema(
     timestamps: true,
   }
 );
- 
+
 CandidateSchema.pre("save", async function (next) {
   console.log("candidate pre save hook called");
   const chars = "0123456789";
   let candidateid = "";
   let isUnique = false;
- 
+
   while (!isUnique) {
     candidateid = "S";
     for (let i = 0; i < 6; i++) {
       candidateid += chars.charAt(Math.floor(Math.random() * chars.length));
     }
- 
+
     const existingCandidate = await Candidate.findOne({ candidateid });
     if (!existingCandidate) {
       isUnique = true;
@@ -163,7 +328,7 @@ CandidateSchema.pre("save", async function (next) {
   this.candidateId = candidateid;
   next();
 });
- 
+
 const Candidate = mongoose.model("Candidate", CandidateSchema);
- 
+
 module.exports = Candidate;
